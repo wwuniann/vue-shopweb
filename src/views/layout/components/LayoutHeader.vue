81b79d78a4cf1e481a3a7f9269b5fa767b75afa8
wwuniann <2269995821@qq.com>
2025-03-05 17:03:12 +0800

    渲染一级导航列表

--- src/views/layout/components/LayoutHeader.vue
+++ src/views/layout/components/LayoutHeader.vue
@@ -1,4 +1,17 @@
 <script setup>
+import { getCategoryAPI } from '@/apis/layout';
+import { onMounted, ref } from 'vue';
+
+const CategoryList= ref([])
+const getCategory = async() => {
+  const res = await getCategoryAPI()
+  CategoryList.value = res.result
+  console.log(CategoryList)
+}
+
+onMounted(() => {
+  getCategory()
+})
 
 </script>
 
@@ -9,12 +22,9 @@
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
       <ul class="app-header-nav">
-        <li class="home">
-          <RouterLink to="/">首页</RouterLink>
+        <li class="home" v-for="item in CategoryList" :key="item.id">
+          <RouterLink to="/">{{ item.name }}</RouterLink>
         </li>
-        <li> <RouterLink to="/">居家</RouterLink> </li>
-        <li> <RouterLink to="/">美食</RouterLink> </li>
-        <li> <RouterLink to="/">服饰</RouterLink> </li>
       </ul>
       <div class="search">
         <i class="iconfont icon-search"></i>
