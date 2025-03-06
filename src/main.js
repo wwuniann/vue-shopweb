5a97c4bad9c2393bd06a44b79a3c6700bbc30575
wwuniann <2269995821@qq.com>
2025-03-06 16:49:38 +0800

    图片懒加载指令实现

--- src/main.js
+++ src/main.js
@@ -21,3 +21,24 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+//定义全局指令
+import { useIntersectionObserver } from '@vueuse/core'
+
+//检测图片是否被监听到
+app.directive('img-lazy', {
+    mounted (el,binding) {
+        //el: 指令绑定的那个元素 img
+        //binding: binding.value 指令等于后面绑定的表达式的值 图片url
+        useIntersectionObserver(
+            el,
+            ([ entry ]) => {
+              console.log(entry.isIntersecting)
+              //返回true则图片被监听到，这时就发送请求获取图片
+              if(entry.isIntersecting){
+                el.src = binding.value
+              }
+            },
+          )
+    }
+    })
