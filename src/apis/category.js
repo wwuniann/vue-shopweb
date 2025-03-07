b12022e60ae709d3a1f96af6a6cac07f5c976b3e
wwuniann <2269995821@qq.com>
2025-03-07 14:59:55 +0800

    面包屑导航渲染

---
+++ src/apis/category.js
@@ -0,0 +1,11 @@
+import httpInstance from "@/utils/http";
+
+export function getCategoryAPI(id){
+    return httpInstance({
+        url:"/category",
+        //请求参数
+        params:{
+            id
+        }
+    })
+}
