5a4ecdd3e19cfab4b3e5bd583206a4ab762b9397
wwuniann <2269995821@qq.com>
2025-03-08 18:11:13 +0800

    小图换大图展示

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,6 +3,7 @@ import { getDetailAPI } from '@/apis/detail';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import DetaildHot from './component/DetaildHot.vue';
+import ImageView from '@/components/ImageView/index.vue'
 
 const goods = ref({})
 const route = useRoute()
@@ -39,7 +40,7 @@ onMounted(() => getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+               <ImageView />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
