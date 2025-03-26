3abdfe5c7c149c50730250efc70998d6c23c1698
wwuniann <2269995821@qq.com>
2025-03-26 23:16:02 +0800

    完善1

--- src/views/CartList/index.vue
+++ src/views/CartList/index.vue
@@ -13,6 +13,9 @@ const singleCheck =(i,selected) =>{
 const allCheck =(selected) =>{
     cartStore.allCheck(selected)
 }
+const delCart = (skuId) =>{
+  cartStore.delCart(skuId)
+}
 
 </script>
 
@@ -60,7 +63,7 @@ const allCheck =(selected) =>{
               </td>
               <td class="tc">
                 <p>
-                  <el-popconfirm title="确认删除吗?" confirm-button-text="确认" cancel-button-text="取消" @confirm="delCart(i)">
+                  <el-popconfirm title="确认删除吗?" confirm-button-text="确认" cancel-button-text="取消" @confirm="delCart(i.skuId)">
                     <template #reference>
                       <a href="javascript:;" >删除</a>
                     </template>
